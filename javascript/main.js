0672da904714be8fba0c4e88ebfa9aee8bb81119
santiagodsgnr <46363598+santiagodsgnr@users.noreply.github.com>
2021-01-14 16:59:14 -0500

    cambios

---
+++ javascript/main.js
@@ -0,0 +1,13 @@
+const app = new Vue ({
+    el: '#app',
+    data() {
+        return {
+            menu: false
+        }
+    },
+    methods: {
+        toggleMenu() {
+            this.menu = !this.menu
+        }
+    }
+})
