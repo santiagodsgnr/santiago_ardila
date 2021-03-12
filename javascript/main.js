ec5243191f0b439c038829371a94eca01b508aea
santiagodsgnr <46363598+santiagodsgnr@users.noreply.github.com>
2021-03-12 08:47:08 -0500

    cambios en form con emailjs y modal form

--- javascript/main.js
+++ javascript/main.js
@@ -3,12 +3,41 @@ const app = new Vue({
     el: '#app',
     data() {
         return {
-            menu: false
+            menu: false,
+            modal: false,
+            name: '',
+            email: '',
+            message: ''
         }
     },
     methods: {
         toggleMenu() {
             this.menu = !this.menu
+        },
+        sendEmail(e) {
+            this.toggleModal()
+            try {
+                emailjs.sendForm('service_csktkor', 'template_7lqbgou', e.target,
+                    'user_eWlhpW6FCgbFI2PzT3HOr', {
+                        name: this.name,
+                        email: this.email,
+                        message: this.message
+                    })
+                console.log('Correo enviado')
+            } catch (error) {
+                console.log({
+                    error
+                })
+            }
+            this.name = '',
+            this.email = '',
+            this.message = ''
+        },
+        toggleModal() {
+            this.modal = true
+            setTimeout(() => {
+                this.modal = false
+            }, 3000);
         }
     }
 })
